b5507568d26e27a53edafb0fe568d244b5e4ec0d
Nick Prokofjev <nprockofjev@yandex.ru>
2021-05-18 21:46:35 +0400

    request validation2

--- middlewares/requestValidation.js
+++ middlewares/requestValidation.js
@@ -13,11 +13,11 @@ const urlValidator = (value) => {
   return value;
 };
 
-const validateId = celebrate({
-  params: Joi.object().keys({
-    _id: Joi.string().alphanum().length(24).hex(),
-  }),
-});
+// const validateId = celebrate({
+//  params: Joi.object().keys({
+//    _id: Joi.string().alphanum().length(24).hex(),
+//  }),
+// });
 
 const validateUpdateCurrentUser = celebrate({
   body: Joi.object().keys({
@@ -65,7 +65,7 @@ const validateSignup = celebrate({
 });
 
 module.exports = {
-  validateId,
+  // validateId,
   validateUpdateCurrentUser,
   validateCreateMovie,
   validateDeleteMovie,
